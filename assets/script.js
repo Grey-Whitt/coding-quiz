c891a6056cf60680d15cca1f27ab55408a233bbe
greyw <greywhitt@gmail.com>
2020-08-21 17:58:42 -0500

    initial buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,41 @@
-var title = document.getElementById('title')
+var title = document.getElementById('title');
+var answers = document.getElementById('answers');
+var startBtn = document.getElementById('start-btn');
+
+var questions = [{
+    ask: 'which one says blue',
+    answer: ['blue', 'blue', 'blue', 'red'],
+    correctAnswer: 'red'
+
+},
+{
+    ask: 'select true',
+    answer: ['true', 'false', 'false', 'false'],
+    correctAnswer: 'true'
+}];
+
+var activeQuestion = 0;
+
 
 // this function starts on game load
-var startApp = function() {
-    
+var onPageLoad = function() {
+    title.textContent = 'Press The Button Below to Start';
+    startBtn.textContent = 'Start';
+
 }
+
+var startQuiz = function() {
+    startBtn.parentNode.removeChild(startBtn)
+    title.textContent = questions[activeQuestion].ask;
+
+    for (var i = 0; i < questions[activeQuestion].answer.length; i++) {
+        var item = document.createElement('li');
+        item.className = 'btn'
+        item.textContent = questions[activeQuestion].answer[i];
+        answers.appendChild(item);
+    }
+}
+
+onPageLoad();
+
+startBtn.addEventListener('click', startQuiz);
