d031c3745b8e5730dfc448234fbc9b496df80dc0
greyw <greywhitt@gmail.com>
2020-08-22 03:48:01 -0500

    kinda works not really though

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,29 @@
 var title = document.getElementById('title');
 var answers = document.getElementById('answers');
 var startBtn = document.querySelector('.start-btn');
+var answerCheck = document.querySelector('.answerCheck')
 var btnId = 0
+var activeQuestion = 0;
+var endQuiz = false;
 
 var questions = [{
     ask: 'which one says red',
-    answer: ['blue', 'blue', 'blue', 'red'],
+    answer: ['blue', 'yellow', 'green', 'red'],
     correctAnswer: '3'
 
 },
 {
     ask: 'select true',
     answer: ['true', 'false', 'false', 'false'],
-    correctAnswer: '1'
+    correctAnswer: '0'
+},
+{
+    ask: 'select ass',
+    answer: ['yes', 'no', 'sometimes', 'ass'],
+    correctAnswer: '3'
 }];
 
-var activeQuestion = 0;
+
 
 
 // this function starts on game load
@@ -27,36 +35,58 @@ var onPageLoad = function() {
 var startQuiz = function() {
     startBtn.parentNode.removeChild(startBtn)
 
-    quiz()
+    quiz(activeQuestion)
     
 }
 
-
-var quiz = function() {
-    //for (i = 0; i < questions.length; i++) {
-        //build question
-        title.textContent = questions[activeQuestion].ask;
+var buildQuestion = function(activeQuestion) {
+    //build question
+    title.textContent = questions[activeQuestion].ask;
         
-        //builds answers with unique id's
-        for (var i = 0; i < questions[activeQuestion].answer.length; i++) {
-            var item = document.createElement('button');
-            item.setAttribute('id', btnId)
-            item.className = 'btn'
-            item.textContent = questions[activeQuestion].answer[i];
-            answers.appendChild(item);
-            btnId++
+    //builds answers with unique id's
+    for (var i = 0; i < questions[activeQuestion].answer.length; i++) {
+        var item = document.createElement('button');
+        item.setAttribute('id', btnId)
+        item.className = 'btn'
+        item.textContent = questions[activeQuestion].answer[i];
+        answers.appendChild(item);
+        btnId++
+    }
+
+    answerQuestion();
+    
+    
+}
+
+var answerQuestion = function(){
+    
+    answers.addEventListener('click', function(){
+        if (event.target.id === questions[activeQuestion].correctAnswer) {
+            
+            answerCheck.textContent = 'Correct!'
+            
+            
+        } else {
+            
+            answerCheck.textContent = 'Incorrect!'
+
         }
+        answers.innerHTML = ''; //Delete ALL CHILDREN
+        
+        activeQuestion++
+        if(activeQuestion != questions.length){
+            quiz(activeQuestion)
+        }
+    })
+    btnId = 0;
+}
 
 
-        answers.addEventListener('click', function(){
-            if (event.target.id === questions[activeQuestion].correctAnswer) {
-                console.log('correct')
-                quiz()
-            } else {
-                console.log('incorrect')
-            }
-        })
-    //}
+
+var quiz = function(actQ) {
+    console.log("Starting quiz with question # " + actQ)
+    buildQuestion(actQ);
+
 }
 
 onPageLoad();
