64ab0391654c93e34312838d7ac8b3e8e93a6f4f
greyw <greywhitt@gmail.com>
2020-08-21 23:54:20 -0500

    fixing buttons

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 var title = document.getElementById('title');
 var answers = document.getElementById('answers');
-var startBtn = document.getElementById('start-btn');
+var startBtn = document.querySelector('.start-btn');
+var btnId = 0
 
 var questions = [{
     ask: 'which one says red',
     answer: ['blue', 'blue', 'blue', 'red'],
-    correctAnswer: 'red'
+    correctAnswer: '3'
 
 },
 {
     ask: 'select true',
     answer: ['true', 'false', 'false', 'false'],
-    correctAnswer: 'true'
+    correctAnswer: '1'
 }];
 
 var activeQuestion = 0;
@@ -21,27 +22,41 @@ var activeQuestion = 0;
 var onPageLoad = function() {
     title.textContent = 'Press The Button Below to Start';
     startBtn.textContent = 'Start';
-
 }
 
 var startQuiz = function() {
     startBtn.parentNode.removeChild(startBtn)
 
+    quiz()
+    
+}
 
-    for (i = 0; i < questions.length; i++) {
-        //builds question
+
+var quiz = function() {
+    //for (i = 0; i < questions.length; i++) {
+        //build question
         title.textContent = questions[activeQuestion].ask;
-
-        //builds answers
+        
+        //builds answers with unique id's
         for (var i = 0; i < questions[activeQuestion].answer.length; i++) {
             var item = document.createElement('button');
+            item.setAttribute('id', btnId)
             item.className = 'btn'
             item.textContent = questions[activeQuestion].answer[i];
             answers.appendChild(item);
+            btnId++
         }
 
-        
-    }
+
+        answers.addEventListener('click', function(){
+            if (event.target.id === questions[activeQuestion].correctAnswer) {
+                console.log('correct')
+                quiz()
+            } else {
+                console.log('incorrect')
+            }
+        })
+    //}
 }
 
 onPageLoad();
