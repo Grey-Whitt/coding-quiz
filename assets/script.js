795d5a030b15a26859aa853af6c1b9b63b1cff4a
greyw <greywhitt@gmail.com>
2020-08-23 00:09:15 -0500

    questions now cycle properly

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,12 @@ var title = document.getElementById('title');
 var answers = document.getElementById('answers');
 var startBtn = document.querySelector('.start-btn');
 var answerCheck = document.querySelector('.answerCheck')
+var time = document.getElementById('timeLeft')
+var endQuiz = false;
 var btnId = 0
 var activeQuestion = 0;
-var endQuiz = false;
 
+//array of questions with possible and correct answer
 var questions = [{
     ask: 'which one says red',
     answer: ['blue', 'yellow', 'green', 'red'],
@@ -13,28 +15,45 @@ var questions = [{
 
 },
 {
-    ask: 'select true',
-    answer: ['true', 'false', 'false', 'false'],
+    ask: 'select e',
+    answer: ['e', 'r', 't', 'y'],
     correctAnswer: '0'
 },
 {
-    ask: 'select ass',
-    answer: ['yes', 'no', 'sometimes', 'ass'],
-    correctAnswer: '3'
+    ask: 'select blue',
+    answer: ['blue', 'false', 'false', 'false'],
+    correctAnswer: '0'
+},
+{
+    ask: 'select yes',
+    answer: ['no', 'no', 'yes', 'no'],
+    correctAnswer: '2'
+},
+{
+    ask: 'select 2',
+    answer: ['1','2','3','4'],
+    correctAnswer: '1'
+},
+{
+    ask: 'select g',
+    answer: ['g','h','j','k'],
+    correctAnswer: '0'
 }];
+
+
 
 
 
 
 // this function starts on game load
 var onPageLoad = function() {
-    title.textContent = 'Press The Button Below to Start';
+    title.textContent = 'Press The Button Below To Start';
     startBtn.textContent = 'Start';
 }
 
 var startQuiz = function() {
     startBtn.parentNode.removeChild(startBtn)
-
+    time.textContent = 'time left:'
     quiz(activeQuestion)
     
 }
@@ -53,18 +72,11 @@ var buildQuestion = function(activeQuestion) {
         btnId++
     }
 
-    answerQuestion();
-    
-    
-}
-
-var answerQuestion = function(){
-    
     answers.addEventListener('click', function(){
+        
         if (event.target.id === questions[activeQuestion].correctAnswer) {
             
             answerCheck.textContent = 'Correct!'
-            
             
         } else {
             
@@ -74,19 +86,18 @@ var answerQuestion = function(){
         answers.innerHTML = ''; //Delete ALL CHILDREN
         
         activeQuestion++
+    
         if(activeQuestion != questions.length){
             quiz(activeQuestion)
         }
     })
     btnId = 0;
+       
 }
 
-
-
 var quiz = function(actQ) {
-    console.log("Starting quiz with question # " + actQ)
-    buildQuestion(actQ);
 
+    buildQuestion(actQ);
 }
 
 onPageLoad();
