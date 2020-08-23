acc87a8892d81a48af392d15d0de544d7a26a4a5
greyw <greywhitt@gmail.com>
2020-08-23 04:51:24 -0500

    now saves and displays highscore

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
 var title = document.getElementById('title');
 var answers = document.getElementById('answers');
-var startBtn = document.querySelector('.start-btn');
+var startBtn = document.getElementById('start');
 var answerCheck = document.querySelector('.answerCheck')
 var time = document.getElementById('timeLeft')
-var endQuiz = false;
+var scoreBoard = document.getElementById('scoreBoard')
+var Restart = document.getElementById('reStart')
+var saveScore = document.getElementById('saveScore')
+var userNameInput = document.getElementById('userName')
 var btnId = 0
 var activeQuestion = 0;
 var timeLeft = 59;
+var end = 1;
+
 
 //array of questions with possible and correct answer
 var questions = [{
@@ -32,12 +37,12 @@ var questions = [{
 },
 {
     ask: 'select 2',
-    answer: ['1','2','3','4'],
+    answer: ['1', '2', '3', '4'],
     correctAnswer: '1'
 },
 {
     ask: 'select g',
-    answer: ['g','h','j','k'],
+    answer: ['g', 'h', 'j', 'k'],
     correctAnswer: '0'
 }];
 
@@ -45,46 +50,57 @@ var questions = [{
 
 
 function countdown() {
-  
+
     // Use setInterval to call a function every second
-    var timeInterval = setInterval(function() {
-      
-      console.log('count')
-      
-      if (timeLeft > 0) {
-        time.textContent = 'Time Remaining: ' + timeLeft 
-        timeLeft--
-      } else {
-        clearInterval(timeInterval)
-        time.textContent = ''
-      }
-  
+    var timeInterval = setInterval(function () {
+
+        if (end === 2) { // if the game is in the end game
+            //stops timer and shows your score
+            clearInterval(timeInterval)
+            time.textContent = 'Your score is: ' + timeLeft
+        } else if (timeLeft === 0) { //if time runs out
+            //stops timer and gives you option to restart
+            clearInterval(timeInterval)
+            time.textContent = 'You ran out of time!'
+            answers.parentNode.removeChild(answers)
+            restart()
+        } else { //if game is not in end game
+            //updates timer by subtracting 1 from timeLeft and displays it
+            time.textContent = 'Time Remaining: ' + timeLeft
+            timeLeft--
+
+        }
+
     }, 1000);
 }
 
 
 
 
-
-
-// this function starts on game load
-var onPageLoad = function() {
-    title.textContent = 'Press The Button Below To Start';
-    startBtn.textContent = 'Start';
+var restart = function () {
+    reStart.setAttribute('class', 'btn start-btn')
+    Restart.textContent = 'Restart?'
 }
 
-var startQuiz = function() {
-    startBtn.parentNode.removeChild(startBtn)
+// this function starts on game load
+var onPageLoad = function () {
+    title.textContent = 'Press The Button To Start!';
+    startBtn.textContent = 'Start';
+    scoreBoard.textContent = 'Current Highscore: ' + localStorage.getItem('username') + ' - ' + localStorage.getItem('score')
+}
+
+var startQuiz = function () {
+    //startBtn.parentNode.removeChild(startBtn)
+    startBtn.setAttribute('class', 'btn start-btn remove')
     time.textContent = 'Time Remaining: 60'
     countdown()
     quiz(activeQuestion)
-    
 }
 
-var buildQuestion = function(activeQuestion) {
+var buildQuestion = function (activeQuestion) {
     //build question
     title.textContent = questions[activeQuestion].ask;
-        
+
     //builds answers with unique id's
     for (var i = 0; i < questions[activeQuestion].answer.length; i++) {
         var item = document.createElement('button');
@@ -95,33 +111,81 @@ var buildQuestion = function(activeQuestion) {
         btnId++
     }
 
-    answers.addEventListener('click', function(){
-        
-        if (event.target.id === questions[activeQuestion].correctAnswer) {
-            
-            answerCheck.textContent = 'Correct!'
-            
-        } else {
-            
-            answerCheck.textContent = 'Incorrect!'
-
-        }
-        answers.innerHTML = ''; //Delete ALL CHILDREN
-        
-        activeQuestion++
-    
-        if(activeQuestion != questions.length){
-            quiz(activeQuestion)
-        }
-    })
+    //calls checkAns function when you click an answer
+    answers.addEventListener('click', checkAns)
     btnId = 0;
-       
+
+
 }
 
-var quiz = function(actQ) {
+//checks to see if your answer is correct
+var checkAns = function () {
+    if (event.target.id === questions[activeQuestion].correctAnswer) {
+        //if correct display correct and move to next question
+        answerCheck.textContent = 'Correct!'
+
+    } else {
+        //if wrong subtract time by 5 and move to next question
+        answerCheck.textContent = 'Incorrect!'
+        decrementTime(5)
+    }
+
+    answers.innerHTML = ''; //Delete ALL CHILDREN
+
+    //moves to next question in questions array
+    activeQuestion++
+
+    // checks to see if you are at last question
+    if (activeQuestion != questions.length) {
+        //if not at last question - continues
+        quiz(activeQuestion)
+    } else {
+        //if at last question moves to endgame
+        endGame()
+    }
+}
+
+//removes 5 from timeLeft
+var decrementTime = function (number) {
+    timeLeft -= 5
+}
+
+var quiz = function (actQ) {
 
     buildQuestion(actQ);
+
 }
+
+var endGame = function () {
+    end++
+    title.textContent = "You've reached the end! Enter your initials to save your score."
+    saveHighScore()
+}
+
+var saveHighScore = function() {
+    saveScore.setAttribute('class', 'btn scoreBtn')
+    userNameInput.classList.remove('remove')
+
+    var userNameInputValue = document.getElementById('userName').value
+
+    saveScore.addEventListener('click', function(){
+        var userNameInputValue = document.getElementById('userName').value
+        console.log('clicked submit')
+        console.log(userNameInputValue)
+        if (userNameInputValue === '') {
+            window.alert('Enter two characters!')
+        } else if (timeLeft > localStorage.getItem('score')) {
+            localStorage.setItem('score', timeLeft)
+            localStorage.setItem('username', userNameInputValue)
+        }
+        saveScore.textContent = 'Score Saved!'
+        restart()
+    })
+    
+
+}
+
+
 
 onPageLoad();
 
